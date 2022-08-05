44c1bccbbbb2e7ff8dd536feaeae4c678fc8eb1e
Dele Omotosho <dele@authoritywit.com>
2022-08-05 12:02:59 -0400

    feat: the on click "google-like" dropdown
    See index.html:L111-113 for our exercise

---
+++ src/app.js
@@ -0,0 +1,14 @@
+// get my data or elements to play with....
+const searchBox = document.getElementById('search-box');
+const results = document.getElementById('results');
+
+// when you get the data, ...... magic....
+searchBox.addEventListener('click', function(){
+
+    // do stuff, e.g. hide or show the block
+    // results.classList.toggle('hidden');
+
+})
+
+// @TODO: see L11 = 113, make the section show up when I click the "comment icon"
+// help solve this problem....
